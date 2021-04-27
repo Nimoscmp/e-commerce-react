44bd27c5e55a2bc4b3c647bbc01a467bde9cc578
nimos <nimos.cmp@gmail.com>
2021-04-27 17:05:46 -0500

    Create redux structure for header

---
+++ src/redux/header/ducks.js
@@ -0,0 +1,23 @@
+/*::::::::::::::::::::: 
+:::  Initial data   :::
+:::::::::::::::::::::::*/
+
+//  Handle focus login inputs
+const initialSelection = {
+    products: true,
+    cart: false
+}
+
+/*::::::::::::::::::::: 
+:::      Types      :::
+:::::::::::::::::::::::*/
+
+const types = {
+    //  Styles focus
+    SELECT_CART: 'SELECT_CART',
+    SELECT_PRODUCTS: 'SELECT_PRODUCTS'
+}
+
+/*::::::::::::::::::::: 
+:::     Reducers    :::
+:::::::::::::::::::::::*/
