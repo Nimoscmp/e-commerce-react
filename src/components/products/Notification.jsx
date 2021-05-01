16f3449fb0c686d01929a5560757faef4dcf01e4
nimos <nimos.cmp@gmail.com>
2021-04-30 22:18:04 -0500

    Make notification responsive

--- src/components/products/Notification.jsx
+++ src/components/products/Notification.jsx
@@ -1,12 +1,15 @@
+import useStyles from "../../styles/Styles"
+import { useMediaQuery } from "@material-ui/core";
 import { DoneRounded } from "@material-ui/icons";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { hide_notif_action } from "../../redux/ducks";
-import useStyles from "../../styles/Styles"
 
 const Notification = () => {
     //  Styles
     const classes = useStyles();
+    const matches_880 = useMediaQuery('(min-width:880px)');
+    const matches_576 = useMediaQuery('(min-width:576px)');
     //  Global states
     const showNotif = useSelector(state => state.notif);
     //  Dispatch
@@ -27,7 +30,8 @@ const Notification = () => {
     <>
         <div
             className={classes.notiDiv}
-            style={{marginLeft: showNotif ? '0' : '-40%'}}>
+            style={{marginLeft: showNotif ? '0' : matches_880 ? '-42%' : matches_576 ? '-65%' : '-90%',
+            width: matches_576 ? '35%' : '75%'}}>
             <h3 className={classes.notiText}>El producto se agregó al carrito</h3>   
             <DoneRounded className={classes.notiIcon}/>
         </div>     
